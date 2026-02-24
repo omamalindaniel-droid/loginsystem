58bb9b64a674383ce3ee97ba4a797d4e982f8f9d
omamalindaniel-droid <omamalindaniel@gmail.com>
2026-02-24 16:33:17 +0800

    Add files via upload

---
+++ drive-download-20260224T080449Z-1-001/app.js
@@ -0,0 +1,88 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { 
+  getFirestore, 
+  setDoc, 
+  doc, 
+  getDoc 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// 🔥 Your Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyDq3OBoyoSGsDDFksKvBQ97SAYJ_6PwlVc",
+  authDomain: "schoolsystem-aura.firebaseapp.com",
+  projectId: "schoolsystem-aura",
+  storageBucket: "schoolsystem-aura.firebasestorage.app",
+  messagingSenderId: "166489049866",
+  appId: "1:166489049866:web:496d34102a2bd6f3bdc8f3",
+  measurementId: "G-10NKC2X6EB"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// ================== REGISTER ==================
+window.register = async function() {
+  const name = document.getElementById("regName").value;
+  const email = document.getElementById("regEmail").value;
+  const password = document.getElementById("regPassword").value;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    // Save additional data in Firestore
+    await setDoc(doc(db, "users", user.uid), {
+      name: name,
+      email: email
+    });
+
+    alert("Registration Successful!");
+    window.location.href = "index.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+// ================== LOGIN ==================
+window.login = async function() {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+// ================== LOGOUT ==================
+window.logout = async function() {
+  await signOut(auth);
+  window.location.href = "index.html";
+}
+
+// ================== PROTECT DASHBOARD ==================
+onAuthStateChanged(auth, async (user) => {
+  if (window.location.pathname.includes("dashboard.html")) {
+    if (user) {
+      const docSnap = await getDoc(doc(db, "users", user.uid));
+      if (docSnap.exists()) {
+        document.getElementById("welcome").innerText =
+          "Welcome " + docSnap.data().name;
+      }
+    } else {
+      window.location.href = "index.html";
+    }
+  }
+});
